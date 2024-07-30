a8123046013304741fc3b54fa714c843736188f6
Satyajit-Sahoo45 <sahoosatyajit2801@gmail.com>
2024-07-30 09:37:12 +0530

    update

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ from dateutil import parser
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://wrDB_owner:qEG4P1mKAVZl@ep-falling-tree-a5x4yzg3.us-east-2.aws.neon.tech/wrDB?sslmode=require"
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DB_URL")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-SECRET_KEY = "alksndlajsdlasdiaisdi9093-49-0934lk234l2j34lkj"
+SECRET_KEY = environ.get("DB_URL")
 
 class User(db.Model):
     id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
